8d0fed26659b43ad48ebc320920ec1512936f021
mirror-ravindu-1 <89637605+mirror-ravindu-1@users.noreply.github.com>
2022-02-05 14:29:41 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -23,12 +23,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n\nProgress 👀: {2}%\n".format(
+        progress = "[{0}{1}] \n\n<b>Progress 👀:</b> {2}%\n".format(
             ''.join(["✦" for i in range(math.floor(percentage / 5))]),
             ''.join(["✧" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "\n{0} of {1}\nSpeed ☘️: {2}/s\n\nETA ⏰: {3}\n".format(
+        tmp = progress + "\n{0} of {1}\n\n<b>Speed ☘️:</b> {2}/s\n\n<b>ETA ⏰:<\n> {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
